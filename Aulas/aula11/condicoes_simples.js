daf543da81e8b8514e43b0765f3349b307ef1dd3
Vitor1973 <devechi.vitor@gmail.com>
2023-02-05 21:34:53 -0300

    Alteração 05 02 2023 II

--- Aulas/aula11/condicoes_simples.js
+++ Aulas/aula11/condicoes_simples.js
@@ -1,7 +1,7 @@
 
-var velocidade = 100
+var velocidade = 79
 console.log(`A velocidade do meu carro na via foi de ${velocidade}Km/h`)
-    if(velocidade > 80) { //CONDIÇÃO SIMPLES!
+    if(velocidade >= 80) { //CONDIÇÃO SIMPLES!
         console.log('VOCÊ EXTRAPOLOU O LIMITE DE VELOCIDADE, MULTADO !!!')
     }
-console.log(`Dirija sempre usando limite de velocidade e na chuva não passe de ${velocidade} por hora.`)
+console.log(`Dirija sempre usando cinto de segurança e na chuva não passe de ${velocidade} por hora.`)
