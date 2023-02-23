c3e35c84dce3b6e7932ec6582a7fabd30cb4e128
Vitor1973 <devechi.vitor@gmail.com>
2023-02-23 00:10:57 -0300

    Alteracao 23 02

--- Aulas/aula11/condicoes_composto.js
+++ Aulas/aula11/condicoes_composto.js
@@ -6,3 +6,33 @@ if(pais != 'Brasil') {
 }else {
     console.log('Você é Brasileiro')
 }
+//Exemplo 1º
+//utilizar a tecla F5 para depurar.
+
+//const semaforo = "verde"
+
+//if (semaforo == "verde") {
+//    console.log("Pode Passar");
+//}
+
+/**  
+// Exemplo 2º 
+const semaforo = "verde"
+
+if (semaforo == "verde") {
+    console.log("Pode Passar")
+} else {
+    console.log("Pare")
+}*/
+
+//Exemplo 3º
+
+const semaforo = 'amarelo'
+
+if (semaforo == "verde") {
+    console.log('Pode Passar')
+} else if (semaforo == 'vermelho') {
+    console.log(`Semaforo ${semaforo} PAREEEE !!!`)
+} else {
+    console.log('Atenção')
+}
--- Aulas/aula11/condicoes_simples.js
+++ Aulas/aula11/condicoes_simples.js
@@ -5,3 +5,12 @@ console.log(`A velocidade do meu carro na via foi de ${velocidade}Km/h`)
         console.log('VOCÊ EXTRAPOLOU O LIMITE DE VELOCIDADE, MULTADO !!!')
     }
 console.log(`Dirija sempre usando cinto de segurança e na chuva não passe de ${velocidade} por hora.`)
+
+var opala = 3
+var gol = 10
+var kwid = 15
+var economico = 15
+
+console.log(`O auto Opala é muito beberrão pois faz ${opala} km/l o Gol faz ${gol} km/l e o Kwid e o campeao de consumo fazendo incriveis ${kwid} km/l por litro de gasolina`)
+
+////utilizar a tecla F5 para depurar.
